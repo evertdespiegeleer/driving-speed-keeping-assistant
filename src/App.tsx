c1770c92ce9c4281ab21241eac2d4cf83f25e987
Evert De Spiegeleer <evert.despiegeleer@rightcrowd.com>
2023-03-04 16:45:58 +0100

    feat: basic functional idea implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,44 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { useGeolocated } from "react-geolocated";
-import './App.css'
+import styled from 'styled-components';
+import { defaultVelocities } from './defaultVelocities';
+
+const StyledApp = styled.div<{
+  overSpeedMps?: number
+}>`
+  background-color: hsl(${p => 120 + Math.min(Math.max((p.overSpeedMps ?? 0) / 3, -1), 1) * 120}, 96%, 79%);
+  transition: background-color 0.2s ease-in-out;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
   const { isGeolocationAvailable, isGeolocationEnabled, coords } = useGeolocated({
     positionOptions: {
-        enableHighAccuracy: false,
+      enableHighAccuracy: false,
     },
     watchPosition: true,
     userDecisionTimeout: 15000,
-});
+  });
+
+  const speedkmph = useMemo(() => coords?.speed && (coords?.speed * 3.6), [coords?.speed])
+
+  const estimatedDesiredSpeedMps = useMemo(() => {
+    const currentSpeed = coords?.speed ?? 0;
+    // Find closest defaultVelocity
+    return defaultVelocities.map((velocity) => {
+      return {
+        diff: Math.abs(currentSpeed - velocity),
+        velocity,
+      }
+    }).sort((a, b) => a.diff - b.diff)[0].velocity;
+  }, [coords?.speed])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => {setCount((count) => count + 1);}}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p>
-          {JSON.stringify({isGeolocationAvailable, isGeolocationEnabled, speed: coords?.speed ?? 0 * 3.6}, null, 2)}
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <StyledApp overSpeedMps={coords?.speed ? (estimatedDesiredSpeedMps - coords?.speed) : 0}>
+      <pre>
+        {JSON.stringify({ isGeolocationAvailable, isGeolocationEnabled, speedkmph: Math.round(speedkmph ?? 0), estimatedDesiredSpeed: Math.round(estimatedDesiredSpeedMps * 3.6) }, null, 2)}
+      </pre>
+    </StyledApp>
   )
 }
 
--- src/defaultVelocities.ts
+++ src/defaultVelocities.ts
@@ -6,4 +6,6 @@ export const defaultVelocities = [ // in m/s
     120
 ].map((velocityKmH) => velocityKmH / 3.6)
 
-export const defaultMaxVelocity = 120 / 3.6;
+export const defaultMaxVelocity = 130 / 3.6; // The app will not register speeds above this value
+
+export const defaultMinVelocity = 20 / 3.6; // The app will not register speeds below this value
