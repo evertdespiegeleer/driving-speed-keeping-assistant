bae416a0e1c2e8e1cc054a0240eb698c54e32e30
Evert De Spiegeleer <evert.despiegeleer@rightcrowd.com>
2023-03-04 17:00:19 +0100

    feat: basic functional idea implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { useMemo, useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useMemo, useState } from 'react'
 import { useGeolocated } from "react-geolocated";
 import styled from 'styled-components';
 import { defaultVelocities } from './defaultVelocities';
+import NoSleep from 'nosleep.js';
 
 const StyledApp = styled.div<{
   overSpeedMps?: number
 }>`
   background-color: hsl(${p => 120 + Math.min(Math.max((p.overSpeedMps ?? 0) / 3, -1), 1) * 120}, 96%, 79%);
   transition: background-color 0.2s ease-in-out;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  .desiredSpeed {
+    font-size: 5em;
+  }
+  pre {
+    /* display: none; */
+  }
 `
 
 function App() {
@@ -20,6 +34,10 @@ function App() {
     userDecisionTimeout: 15000,
   });
 
+  useEffect(() => {
+    const noSleep = new NoSleep();
+    noSleep.enable();
+  }, []);
   const speedkmph = useMemo(() => coords?.speed && (coords?.speed * 3.6), [coords?.speed])
 
   const estimatedDesiredSpeedMps = useMemo(() => {
@@ -38,6 +56,9 @@ function App() {
       <pre>
         {JSON.stringify({ isGeolocationAvailable, isGeolocationEnabled, speedkmph: Math.round(speedkmph ?? 0), estimatedDesiredSpeed: Math.round(estimatedDesiredSpeedMps * 3.6) }, null, 2)}
       </pre>
+      <div className="desiredSpeed">
+        {Math.round(estimatedDesiredSpeedMps * 3.6)}
+      </div>
     </StyledApp>
   )
 }
